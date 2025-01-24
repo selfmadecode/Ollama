78c92a706939684ed20123b4fe80d217819d4039
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-24 21:49:38 +0100

    register AI services

--- src/Ollama.API/ServiceRegistration.cs
+++ src/Ollama.API/ServiceRegistration.cs
@@ -4,6 +4,26 @@ namespace Ollama.API;
 
 public static class ServiceRegistration
 {
-    
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        builder.AddAIServices();
+    }
+
+    public static void AddAIServices(this IHostApplicationBuilder builder)
+    {
+        var loggerFactory = builder.Services.BuildServiceProvider().GetService<ILoggerFactory>();
+
+        string? ollamaEndpoint = builder.Configuration["AI:Ollama:Endpoint"];
+
+        if (!string.IsNullOrWhiteSpace(ollamaEndpoint))
+        {
+            builder.Services.
+                AddChatClient(new OllamaChatClient(ollamaEndpoint, builder.Configuration["AI:Ollama:ChatModel"] ?? "llama3.1"))
+                .UseFunctionInvocation()
+                .UseOpenTelemetry(configure: t => t.EnableSensitiveData = true)
+                .UseLogging(loggerFactory)
+                .Build();
+        }
+    }
 }
 
