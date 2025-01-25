247576e7785f3424b6bf09bea7e3274a9f3e833d
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-25 12:33:53 +0100

    register AI services

--- src/Ollama.API/Program.cs
+++ src/Ollama.API/Program.cs
@@ -9,9 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.AddApplicationServices();
 
 //builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
