96b562e16b0ba0988b1c814452fda762f82035c0
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-02-03 08:01:28 +0100

    add IChatService interface and implementation

---
+++ src/Ollama.API/Interfaces/IChatService.cs
@@ -0,0 +1,7 @@
+﻿namespace Ollama.API.Interfaces;
+
+public interface IChatService
+{
+    Task<ChatCompletion> CompleteChat(ChatPrompt prompt);
+    Task<List<ChatMessage>> ChatHistory(ChatPrompt prompt);
+}
---
+++ src/Ollama.API/Services/ChatService.cs
@@ -0,0 +1,35 @@
+﻿namespace Ollama.API;
+
+internal sealed class ChatService : IChatService
+{
+    private readonly IChatClient _chatClient;
+    public ChatService(IChatClient chatClient)
+    {
+        _chatClient = chatClient;
+    }
+
+    public async Task<List<ChatMessage>> ChatHistory(ChatPrompt prompt)
+    {
+        var messages = GroundPrompt(prompt);
+
+        var response = await _chatClient.CompleteAsync(messages);
+        messages.Add(new ChatMessage(ChatRole.Assistant, response.Message.Contents));
+
+        return messages;
+    }
+
+    public async Task<ChatCompletion> CompleteChat(ChatPrompt prompt)
+    {
+        var messages = GroundPrompt(prompt);
+
+        return await _chatClient.CompleteAsync(messages);
+    }
+
+    private List<ChatMessage> GroundPrompt(ChatPrompt chatPrompt)
+    {
+        return
+        [
+            new ChatMessage(ChatRole.User, chatPrompt.Message)
+        ];
+    }
+}
